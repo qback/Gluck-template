072005baefbbfc5e822dd372fd0f017e929d8500
Oleg <qbacknn@gmail.com>
2017-02-12 10:06:06 +0300

    feat(scripts): init slider-carousel for Step block

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,34 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'slick-carousel';
 
 $(() => {
 	svg4everybody();
+
+	/* activate slick-carousels inside Steps block */
+
+	const selectors = ['#step1', '#step2', '#step3'];
+
+	selectors.forEach( function (element, index) {
+		$(element).slick({
+			fade: true,
+			dots: false,
+			draggable: false,
+			initialSlide: index,
+			prevArrow: $(element + ' ~ .steps__arrow_prev'),
+			nextArrow: $(element + ' ~ .steps__arrow_next'),
+			autoplay: false,
+			swipe: false,
+			speed: 300,
+			slidesToShow: 1,
+			responsive: [{
+				breakpoint: 769,
+				settings: {
+					arrows: false,
+				}
+
+			}]
+		});
+	});
+
 });
